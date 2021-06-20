9be2c80f6a4b3caa27347bef93df90f527209843
Sakthi <77956026+kashykashh@users.noreply.github.com>
2021-06-20 13:22:06 +0800

    Add files via upload

---
+++ go_goolge.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://www.google.com/")
+print(driver.title)
+
+search = driver.find_element_by_name("q")
+search.send_keys("Python")
+search.send_keys(Keys.RETURN)
+
+
+# time.sleep(5)
+
+# driver.quit()
